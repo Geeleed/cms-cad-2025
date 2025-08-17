1171b729eb015a2725d0007753dbedfd2e7a9970
Geeleed <surasak.kaewpho@gmail.com>
2025-08-17 12:03:33 +0700

    update loading

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,18 +4,11 @@ export default function Loading() {
   return (
     <div
       style={{
-        width: "100%",
-        height: "100%",
+        height: "100vh",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        zIndex: 1,
-        backgroundColor: "white",
-        // backdropFilter: "blur(10px)",
       }}
     >
       <div
