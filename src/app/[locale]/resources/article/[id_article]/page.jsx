a488df8b593927541d48119194b6e105478a3cc1
Surasak Kaewpho <surasak.kaewpho@gmail.com>
2026-04-04 05:35:36 +0700

    เอา ; ที่เกินออก

--- src/app/[locale]/resources/article/[id_article]/page.jsx
+++ src/app/[locale]/resources/article/[id_article]/page.jsx
@@ -5,14 +5,14 @@ export default async function Page({ params }) {
   const { id_article } = await params;
 
   if (!id_article) {
-    return <div>ไม่พบบทความ</div>;
+    return <div>ไม่พบบทความ</div>
   }
 
   const result = await getArticleWithIdArticle(id_article);
   return (
     <main className="py-[8rem]">
       <div className="max-w-[1250px] mx-auto w-full">
-        <div dangerouslySetInnerHTML={{ __html: result.content }} />;
+        <div dangerouslySetInnerHTML={{ __html: result.content }} />
       </div>
     </main>
   );
