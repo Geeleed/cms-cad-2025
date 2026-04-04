66b8de8dfa2d3e84deeb44dfec18b9d2d0afbc32
Surasak Kaewpho <surasak.kaewpho@gmail.com>
2026-04-05 00:29:18 +0700

    ตั้ง .yaml

--- src/api/path.js
+++ src/api/path.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   typeof window === "undefined"
-    ? `${process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"}/api`
+    ? `http://localhost:${process.env.PORT || 3000}/api`
     : "/api";
 
 export const apiPath = {
